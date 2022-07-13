75a57d0305571dd427e7b3b8dd7d792b0d5fa648
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-13 11:51:08 -0400

    initiate db

--- server/index.js
+++ server/index.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
-const PORT = 5000
+const cors = require("cors")
+const pool = require("./db")
+
+const PORT = 5050
+
+//middleware
+app.use(cors())
+app.use(express.json());
+
+//ROUTES//
+
+//create a user
+
+app.post("/user", async(req, res) => {
+  try {
+    console.log(req.body)
+  } catch (err) {
+    console.error(err.message)
+  }
+})
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`)
