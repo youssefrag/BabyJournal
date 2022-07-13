12eba73f27d809380d1baccb254ede04b5d38e8b
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-13 14:25:53 -0400

    initiate registration page

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,16 @@
 import { Routes, Route, Navigate } from "react-router-dom"
 import { Button } from '@mui/material'
 import axios from 'axios'
+import { useNavigate } from 'react-router-dom';
+import RegistrationPage from "./components/RegistrationPage";
 
 function App() {
 
   return (
     <div className="App">
-      <Button
-        variant="contained"
-      >
-        Register
-      </Button>
+      <Routes>
+        <Route path="/" element = {<RegistrationPage />} />
+      </Routes>
     </div>
   );
 }
