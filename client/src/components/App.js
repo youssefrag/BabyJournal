d8102be8a2fe231e296ff830e8ee9c8dfc4147db
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-15 19:02:45 -0400

    add top margin to app to make room for navbar

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,17 @@ import { useNavigate } from 'react-router-dom';
 import RegistrationPage from "./ui/RegistrationPage";
 import LoginPage from "./ui/LoginPage";
 
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: '80px',
+  },
+});
+
 function App() {
+
+  const classes = useStyles();
 
   return (
     <div className="App">
