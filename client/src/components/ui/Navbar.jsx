834b3ffe5a34f56ca30e7abae4eeb9576efd7920
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-15 19:23:39 -0400

    fix styles installation bug

--- client/src/components/ui/Navbar.jsx
+++ client/src/components/ui/Navbar.jsx
@@ -1,24 +1,23 @@
 import {Button, Typography, AppBar, Toolbar } from '@mui/material';
 import { makeStyles } from "@mui/styles";
 
-// const useStyles = makeStyles({
-//   tool: {
-//     width:'100%',
-//     display: 'inline-flex',
-//     justifyContent: 'space-between',
-//   },
-// })
+const useStyles = makeStyles({
+  tool: {
+    width:'100%',
+    display: 'inline-flex',
+    justifyContent: 'space-between',
+  },
+})
 
 export default function Navbar() {
 
-  // const classes = useStyles()
+  const classes = useStyles()
 
   return (
     <AppBar 
       color='primary'
     >
-      <Toolbar>
-      {/* <Toolbar className={classes.tool}> */}
+      <Toolbar className={classes.tool}>
         <Button
           color="secondary"
           variant='contained' 
