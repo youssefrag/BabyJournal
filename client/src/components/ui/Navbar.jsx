734064a86f3af95dec11da49e3645f1960988db2
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-18 13:26:53 -0400

    adjust navbar for logged in state

--- client/src/components/ui/Navbar.jsx
+++ client/src/components/ui/Navbar.jsx
@@ -1,5 +1,7 @@
+import React, { useContext } from "react";
 import {Button, Typography, AppBar, Toolbar } from '@mui/material';
 import { makeStyles } from "@mui/styles";
+import { UserContext } from "../../context/userContext"
 
 const useStyles = makeStyles({
   tool: {
@@ -13,42 +15,79 @@ export default function Navbar() {
 
   const classes = useStyles()
 
-  return (
-    <AppBar 
-      color='primary'
-    >
-      <Toolbar className={classes.tool}>
-        <Button
-          color="secondary"
-          variant='contained' 
-          size='large' 
-          // onClick={() => navigate("/login")}
-        >
-          Baby Page!
-        </Button>
-        <Typography variant="h4">
-          BabyJournal
-        </Typography>
-        <div>
+  const { isUserLoggedIn, setUserLoggedIn } = useContext(UserContext);
+
+  if (isUserLoggedIn === true) {
+    return(
+      <AppBar 
+        color='primary'
+      >
+        <Toolbar className={classes.tool}>
           <Button
             color="secondary"
             variant='contained' 
             size='large' 
             // onClick={() => navigate("/login")}
           >
-            Login
+            Baby Page!
           </Button>
+          <Typography variant="h4">
+            Welcome username
+          </Typography>
+          <div>
+            <Button
+              color="secondary"
+              style={{ marginLeft: 10, marginRight: 50}}
+              variant='contained' 
+              size='large' 
+              // onClick={() => navigate("/register")}
+            >
+              Logout
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+
+    )
+  } else {
+    return (
+      <AppBar 
+        color='primary'
+      >
+        <Toolbar className={classes.tool}>
           <Button
             color="secondary"
-            style={{ marginLeft: 10, marginRight: 50}}
             variant='contained' 
             size='large' 
-            // onClick={() => navigate("/register")}
+            // onClick={() => navigate("/login")}
           >
-            Register
+            Baby Page!
           </Button>
-        </div>
-      </Toolbar>
-    </AppBar>
-  )
+          <Typography variant="h4">
+            BabyJournal
+          </Typography>
+          <div>
+            <Button
+              color="secondary"
+              variant='contained' 
+              size='large' 
+              // onClick={() => navigate("/login")}
+            >
+              Login
+            </Button>
+            <Button
+              color="secondary"
+              style={{ marginLeft: 10, marginRight: 50}}
+              variant='contained' 
+              size='large' 
+              // onClick={() => navigate("/register")}
+            >
+              Register
+            </Button>
+          </div>
+        </Toolbar>
+      </AppBar>
+    )
+  }
+
 }
