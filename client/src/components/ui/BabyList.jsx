fccbc18b683ff31d1d63e4bf3542f80f0134688a
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-18 17:07:19 -0400

    initiate baby list item component

--- client/src/components/ui/BabyList.jsx
+++ client/src/components/ui/BabyList.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, { useContext } from "react";
+import { useState, useEffect } from "react";
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 export default function BabyList() {
   return (
