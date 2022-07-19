88155468152e2885d96702e011f35d82a6f86fdb
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-19 17:10:40 -0400

    create baby state in new baby page

--- client/src/components/ui/NewBaby.jsx
+++ client/src/components/ui/NewBaby.jsx
@@ -1,11 +1,36 @@
 import React, { useState } from 'react';
+import { useNavigate } from "react-router-dom";
+
 import { Stack, TextField } from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
+import moment from 'moment';
 
 export default function NewBaby() {
 
-  const [selectedDate, setSelectedDate] = useState(null)
-  console.log( {selectedDate })
+  let navigate = useNavigate();
+
+  const [baby, setBaby] = useState({
+    first_name: '',
+    last_name: '',
+    date_of_birth: '',
+    birth_location: '',
+    picture_url: ''
+  })
+
+  const handleChange = (e) => {
+    console.log(baby);
+
+    const name = e.target.name
+    const value = e.target.value
+    setBaby(prev => ({...baby, [name]: value}))
+  }
+
+  const [date, setDate] = useState(() => moment())
+  const handleDateChange = (e) => {
+    setDate(e.date);
+ }
+
+ console.log(date)
 
   return (
     <Stack
@@ -15,9 +40,9 @@ export default function NewBaby() {
       <DatePicker 
         label='Date of Birth' 
         renderInput={(params) => <TextField {...params}/>}
-        value={selectedDate}
+        value={date}
         onChange={(newValue) => {
-          setSelectedDate(newValue)
+          setDate(newValue)
         }}
       />
     </Stack>
