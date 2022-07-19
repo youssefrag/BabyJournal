e11e1bf20e3adee1963187d9499a95e3a651acb6
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-19 12:12:01 -0400

    build baby list item and add baby avatar

--- client/src/components/ui/BabyListItem.jsx
+++ client/src/components/ui/BabyListItem.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Typography } from '@mui/material';
 
-export default function BabyListItem() {
-  return (
-    <div>BabyListItem</div>
-  )
+export default function BabyListItem(props) {
+
+  const { name, image, id, goToNewBabyPage } = props
+
+  if (name) {
+    return (
+      <div
+        onClick={goToNewBabyPage}
+      >
+        { (image)
+          ? <img src={image} />
+          : <img src='/../public/baby-avat.webp' />
+        }
+      </div>
+    )
+  }
 }
