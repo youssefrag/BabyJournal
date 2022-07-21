cf90fd815242995addb301cbe8cb540fcf3c38f4
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-21 13:23:32 -0400

    add log types and units to baby details page

--- client/src/components/ui/BabyDetailsPage.jsx
+++ client/src/components/ui/BabyDetailsPage.jsx
@@ -3,6 +3,33 @@ import { useParams } from "react-router-dom";
 
 export default function BabyDetailsPage() {
 
+  const LogTypes = {
+    HEAD: "head",
+    HEIGHT: "height",
+    WEIGHT: "weight",
+    TEMPERATURE: "temperature",
+    MEDICINE: "medicine",
+    VACCINE: "vaccine",
+    APPOINTMENT: "appointment"
+  }
+  const lengthUnits = [
+    {name: "Centimeter", value: "cm"}, 
+    {name: "Millimeter", value: "mm"},
+    {name: "Inch", value: "in"},
+    {name: "Foot", value: "ft"}
+  ]
+  const temperatureUnits = [
+    {name: "Celcius", value: "C"}, 
+    {name: "Farenheit", value: "F"}
+  ]
+  const weightUnits = [
+    {name: "kilogram", value: "kg"},
+    {name: "gram", value: "g"}, 
+    {name: "milligram", value: "mg"},
+    {name: "ounce", value: "oz"},
+    {name: "pound", value: "lb"}
+  ]
+
   const { id } = useParams()
 
   return (
