75b6c711740e79f29fcdb04e8680508fe8891fb2
Youssef Ragab <ragab.youssef95@gmail.com>
2022-07-25 12:25:33 -0400

    remove time picker

--- client/src/components/ui/LogMeasurementModal.jsx
+++ client/src/components/ui/LogMeasurementModal.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 
 import { Typography, Box, TextField, Button } from '@mui/material'
 import { DatePicker } from '@mui/x-date-pickers';
-import { TimePicker } from '@mui/x-date-pickers';
 
 import moment from 'moment';
 import axios from 'axios';
@@ -18,11 +17,10 @@ export default function LogGrowthModal(props) {
   const [measurement, setMasurement] = useState({
     type: measurementType,
     amount: '',
-    date_time: '',
+    date: '',
   })
 
   const [date, setDate] = useState(() => moment())
-  const [time, setTime] = useState(() => moment())
 
   const handleChange = (e) => {
     const name = e.target.name
@@ -35,26 +33,23 @@ export default function LogGrowthModal(props) {
     if (!amount) {
       alert('Empty values')
     }
-    const dateTimeStr = date.format("YYYY-MM-DD") + "T" + time.format("HH:mm:ssZ");
-    const dateTime = moment(dateTimeStr);
-    measurement['datetime'] = parseInt(dateTime.format("X"));
+    measurement['date'] = date.toString().slice(0, 15)
     console.log(measurement)
-    // measurement['date'] = date.toString().slice(0, 15)
-    // axios.post(`http://localhost:5050/log/measurement/${babyId}`, measurement, {
-    //   withCredentials: true,
-    // })
-    // .then(() => {
-    //   if (type === 'head') {
-    //     handleCloseHeadLog()
-    //   } else if (type === 'weight') {
-    //     handleCloseWeightLog()
-    //   } else if (type === 'height') {
-    //     handleCloseHeightLog()
-    //   }
-    // })
-    // .catch((err) => {
-    //   console.log(err.message)
-    // })
+    axios.post(`http://localhost:5050/log/measurement/${babyId}`, measurement, {
+      withCredentials: true,
+    })
+    .then(() => {
+      if (type === 'head') {
+        handleCloseHeadLog()
+      } else if (type === 'weight') {
+        handleCloseWeightLog()
+      } else if (type === 'height') {
+        handleCloseHeightLog()
+      }
+    })
+    .catch((err) => {
+      console.log(err.message)
+    })
   }
 
   let header = ''
@@ -94,14 +89,6 @@ export default function LogGrowthModal(props) {
           onChange={(newValue) => {
             setDate(newValue)
           }}
-        />
-        <TimePicker
-          label="Basic example"
-          value={time}
-          onChange={(newValue) => {
-            setTime(newValue);
-          }}
-          renderInput={(params) => <TextField {...params} />}
         />
       </Box>
       <Button
